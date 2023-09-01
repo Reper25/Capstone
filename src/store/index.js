56737d844763b98ebae89e491d85b6ac6c19541c
laeeq <laeeq.majal@gmail.com>
2023-09-01 10:12:21 +0200

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,114 @@
 import { createStore } from 'vuex'
+import axios from "axios";
 const miniURL = "https://capstone2-0put.onrender.com";
 export default createStore({
   state: {
+    users: null,
+    user: null,
+    products: null,
+    product: null,
+    spinner: null,
+    token: null,
+    selectedProduct: null,
+    msg: null,
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    setUsers(state, users) {
+      state.users = users;
+    },
+    setUser(state, user) {
+      state.user = user;
+    },
+    setProducts(state, products) {
+      state.products = products;
+      console.log(products)
+    },
+    setSelectedProduct(state, product) {
+      state.selectedProduct = product
+    },
+    setProduct(state, product) {
+      state.product = product;
+    },
+    setSpinner(state, spinner) {
+      state.spinner = value;
+    },
+    setToken(state, token) {
+      state.token = token;
+    },
+    setMsg(state, msg) {
+      state.msg = msg;
+    },
   },
   actions: {
+    async fetchUsers(context) {
+      try {
+        const { data } = await axios.get(`${miniURL}Users`);
+        context.commit("setUsers", data.results);
+      } catch (e) {
+        context.commit("setMsg", "an error occured");
+      }
+    },
+    async fetchUser(context) {
+      try {
+        const { data } = await axios.get(`${miniURL}Users`);
+        context.commit("setUser", data.results);
+      } catch (e) {
+        context.commit("setMsg", "an error occured");
+      }
+    },
+    async createUser(context) {
+      try{
+        const { data } = await axios.post(`${miniURL}Users`)
+        context.commit("setUser", data.results);
+      } catch (e) {
+        context.commit("setMsg", "an error occured");
+      }
+    },
+    async updateUser(context) {
+      try{
+        const { data } = await axios.patch(`${miniURL}Users`)
+        context.commit("setUser", data.results);
+      } catch (e) {
+        context.commit("setMsg", "an error occured");
+      }
+    },
+    async deleteUser(context) {
+      try{
+        const { data } = await axios.delete(`${miniURL}Users`)
+        context.commit("setUser", data.results);
+      } catch (e) {
+        context.commit("setMsg", "an error occured");
+      }
+    },
+    async fetchProducts(context) {
+      try{
+        let products = await (await fetch(" ")).json()
+        if (products) {
+          context.commit ("setProducts", products)
+        } else {
+          alert("error")
+        }
+      }
+      catch(e) {
+        console.error(error)
+      }
+    }
   },
-  modules: {
-  }
-})
+  async addProduct(context, payload) {
+    console.log("REACHED");
+      try {
+        const { res } = await axios.post(`${miniURL}product`, payload);
+        const { results, err } = await res.data;
+        if (results) {
+          context.commit("setProduct", results[0]);
+          context.commit("setSpinner", false);
+        } else {
+          context.commit("setMsg", msg);
+        }
+      } catch (e) {
+        context.commit("setMsg", "an error occured");
+      }
+    },
+  },
+);
