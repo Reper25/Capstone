180f3153ad5050903e7fe7923c18e73350bca855
laeeq <laeeq.majal@gmail.com>
2023-09-07 13:50:16 +0200

    small thing

--- model/users.js
+++ model/users.js
@@ -34,7 +34,7 @@ class Users{
     }
     async register(req, res) {
         const data = req.body;
-        data.userPass = await hash(data.userPass, 16);
+        data.userPass = await hash(data.userPass, 10);
         const user = {
           emailAdd: data.emailAdd,
           userPass: data.userPass,
